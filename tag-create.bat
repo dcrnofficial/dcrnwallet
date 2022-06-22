3dbb9cedeced95bfb0d09b4bb4a05ef4a13ca03f
d-opr <dopr@gmail.com>
2022-06-22 16:00:19 +0800

    add dcrnwallet to v8

--- tag-create.bat
+++ tag-create.bat
@@ -1,7 +1,8 @@
-set version=v8.0.8
+set version=v8.0.0
 git tag -a rpc/jsonrpc/types/version14/%version% -m "%version%"
 git tag -a rpc/client/dcrd/version1/%version% -m "%version%"
 git tag -a deployments/version2/%version% -m "%version%"
 git tag -a errors/version2/%version% -m "%version%"
 git tag -a validate/version1/%version% -m "%version%"
+git tag -a wallet/version3/%version% -m "%version%"
 git push origin --tags
