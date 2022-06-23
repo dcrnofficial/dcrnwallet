79c3c00f50d9ce72274624cd3d421d538ad6d921
paoner <paoner@gmail.com>
2022-06-23 17:39:55 +0800

    tag tests

--- rpc/jsonrpc/types/version14/doc.go
+++ rpc/jsonrpc/types/version14/doc.go
@@ -1,4 +1,4 @@
-// tags pull test
+// tags pull test 1111
 // Copyright (c) 2019 The Decred developers
 // Use of this source code is governed by an ISC
 // license that can be found in the LICENSE file.
